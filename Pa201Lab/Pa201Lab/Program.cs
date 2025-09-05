c4065cda67750b89a18eee594d8fa8c1f40aac7c
Samir A. Almammadli <samiraa@code.edu.az>
2025-09-05 15:33:23 +0400

    csharp commit

---
+++ Pa201Lab/Pa201Lab/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//string name = "Samir";
+//Console.WriteLine("Salam "+name);
+//int num = 0;
+//if (num > 0)
+//{
+//    Console.WriteLine("Positive");
+//}else if(num < 0)
+//{
+//    Console.WriteLine("Negative");
+//}else
+//{
+//    Console.WriteLine("Zero");
+//}
+
+int age = 18;
+if (age > 0 && age < 18)
+{
+    Console.WriteLine("Usaq");
+}
+else if (age >= 18 && age <= 65)
+{
+    Console.WriteLine("Yetiskin");
+}
+else if(age > 65)
+{
+    Console.WriteLine("Yasli");
+}
