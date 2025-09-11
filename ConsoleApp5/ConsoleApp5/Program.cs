c9e8669144c492b5b0db2b2cead0ebf575bedd80
Smr-25 <samiraa@code.edu.az>
2025-09-11 17:36:16 +0400

    Change num value from 12 to 10

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -9,7 +9,7 @@ for (int i = 1; i <= n; i++)
 }
 
 int[] number = {1,2,3,4,5, 6, 7, 8, 9 };
-int num = 12;
+int num = 10;
 int[] number2 = new int[number.Length + 1];
 for (int i = 0; i < number.Length; i++)
 {
