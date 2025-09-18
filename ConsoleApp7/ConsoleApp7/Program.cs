e6406a9468df3a2fdbef3fc938105d8eff9574c7
Samir A. Almammadli <samiraa@code.edu.az>
2025-09-18 17:32:01 +0400

    new tasks 18.09.23

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,19 @@
+﻿static string DomainReturn(string email)
+{
+    string domain = "";
+    for (int i = 0; i < email.Length; i++)
+    {
+        if (email[i] == '@')
+        {
+            domain += email.Substring(i + 1);
+            break;
+        }
+    }
+
+    domain += email.Split('@')[1];
+
+    return domain;
+}
+
+Console.WriteLine(DomainReturn("test@code.edu.az"));
+
