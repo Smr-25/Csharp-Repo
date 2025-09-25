8ebe6f59082857373fafef4c10c6273f8638ca29
Samir A. Almammadli <samiraa@code.edu.az>
2025-09-25 17:25:58 +0400

    25.09.25 classroom task

---
+++ ConsoleApp9/ConsoleApp9/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp9
+{
+    internal class Car
+    {
+        static int n = 0;
+        public Car()
+        {
+            n++;
+            Console.WriteLine(n);
+      
+        }
+       
+        
+
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleApp9;
+
+Car car = new Car();
+Car car1 = new Car();
+Car car2 = new Car();
+Car car3 = new Car();
