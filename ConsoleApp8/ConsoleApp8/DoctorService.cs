027a7fd429a3b3f62e3c6e5fdbd6e635e18537a1
Smr-25 <samiraa@code.edu.az>
2025-09-28 12:19:17 +0400

    Fix formatting in SearchByFullName method

--- ConsoleApp8/ConsoleApp8/DoctorService.cs
+++ ConsoleApp8/ConsoleApp8/DoctorService.cs
@@ -43,14 +43,14 @@ namespace ConsoleApp8
 
 
         public void SearchByFullName(Doctor[] doctorArray ,string searchingText)
-        {
+        {           
             foreach (var doctor in doctorArray)
             {
                 if (doctor.FullName.Contains(searchingText.Trim(), StringComparison.OrdinalIgnoreCase))
                 {
-
                     Console.WriteLine($"FullName: {doctor.FullName}, Age: {doctor.Age}, Email: {doctor.Email}, Address: {doctor.Address}");
                 }
+               
             }
         }
 
