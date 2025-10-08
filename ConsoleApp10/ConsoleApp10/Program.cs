1c935728c6d57957a51672bfdbe8b3a64b84bfbd
Smr-25 <samiraa@code.edu.az>
2025-10-08 12:05:22 +0400

    Add books to library and show book info
    Added multiple books to the library and displayed info for one.

--- ConsoleApp10/ConsoleApp10/Program.cs
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -6,3 +6,23 @@ library.AddBook(new Book("Book1", 10, "A1", 100));
 library.AddBook(new Book("Book1", 10, "A1", 100));
 
 Console.WriteLine(library.GetBookById(1)); 
+Library library = new(3);
+
+library.AddBook(new Book("Book1", 10, "A1", 100));
+library.AddBook(new Book("Book2", 20, "A1", 200));
+library.AddBook(new Book("Book3", 30, "A1", 300));
+
+
+
+library.GetBookById(2).ShowInfo();
+
+
+//Book book = new Book("Book1", 10, "A1", 100);
+//book.Count = 5;
+//book.Price = 100;
+//book.Sell();
+//book.Sell();
+//book.Sell();
+//book.Sell();
+//book.Sell();
+//book.Sell();
