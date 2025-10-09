372ba66c62dce29459df0631c50cd52571826a1d
Smr-25 <samiraa@code.edu.az>
2025-10-09 09:03:32 +0400

    Implement employee management menu
    Added a menu for employee management with options to add, get, and remove employees.

--- ConsoleApp11/ConsoleApp11/Program.cs
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -14,4 +14,33 @@ if (!File.Exists(path))
     File.Create(path);
 
 Department department = new();
-department.AddEmployee();
+
+while (true)
+{
+    Console.WriteLine();
+    Console.WriteLine("Menu");
+    Console.WriteLine("1. Add employee");
+    Console.WriteLine("2. Get employee by id");
+    Console.WriteLine("3. Remove employee");
+    Console.WriteLine("0. Quit");
+    Console.WriteLine();
+    Console.WriteLine("Please Choose Option");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            department.AddEmployee();
+            break;
+        case 2:
+            department.GetEmployeeById().ShowInfo();
+            break;
+        case 3:
+            department.RemoveEmployee();
+            break;
+        case 0:
+            return;
+        default:
+            Console.WriteLine("Choose right option");
+            break;
+    }
+}
