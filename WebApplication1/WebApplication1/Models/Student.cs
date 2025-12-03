c24482b87128f671cf94ddc4d754634757250fbe
Smr-25 <samiraa@code.edu.az>
2025-12-03 11:22:25 +0400

    02.12.25 task v2

--- WebApplication1/WebApplication1/Models/Student.cs
+++ WebApplication1/WebApplication1/Models/Student.cs
@@ -4,8 +4,6 @@ public class Student
 {
     public int Id { get; set; }
     public string FirstName { get; set; }
-    = string.Empty;
-    public string LastName { get; set; }
-    = string.Empty;
+    public int Age { get; set; }
 
 }
